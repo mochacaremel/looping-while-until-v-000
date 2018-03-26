59831c016df2d1ceca29c1f1bfdcd25a74239727
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-26 20:47:34 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,9 @@
 def using_until
         levitation_force = 6
-        
-        
+    until counter == 20
+      puts "The current number is less than 20."
+      counter += 1
+    end
         
         
         
